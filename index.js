e0066371a6fda81422057a351f018f5c4b2593aa
de-sipher <0mu5sfm4@anonaddy.me>
2022-10-05 14:25:59 +0530

    fixing broked git files

---
+++ index.js
@@ -0,0 +1,97 @@
+import mysql from "mysql2"
+import cors from "cors"
+import express from "express"
+import dotenv from "dotenv"
+
+const dot = dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const PORT = process.env.PORT || 8080
+
+const db = mysql.createConnection(process.env.DATABASE_URL)
+// db.end() // To drop the connection. need to check on usage details.
+
+// Config to connect to localhost
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "todos",
+// })
+db.connect((err) => {
+    err
+        ? console.log(
+              "Error encountered while connecting to mySQL database.",
+              err
+          )
+        : console.log(
+              "Connection to mySQL established.",
+              "Connected to PlanetScale!"
+          )
+})
+
+// Send every item as a response
+app.get("/todos", (req, res) => {
+    let sqlReqText = `SELECT * FROM items`
+    db.query(sqlReqText, (err, result) => {
+        if (err) {
+            console.log("Error while querying database.")
+            throw err
+        }
+        let newRes = result.map((obj, index) => {
+            obj.isComplete = obj.isComplete == 1
+            obj.isHidden = obj.isHidden == 1
+            return obj
+        })
+        res.status(200).send(newRes)
+    })
+})
+
+// Add a single item to the database
+app.post("/todos", (req, res) => {
+    if ((req.body.length > 1) | (req.body.length < 1)) res.status(400)
+    let data = req.body
+    let sqlInsertQuery = `INSERT items (uuid, todoText, isComplete, isHidden) VALUES(?, ?, ?, ?)`
+    db.query(
+        sqlInsertQuery,
+        [data.uuid, data.todoText, data.isComplete, data.isHidden],
+        (err) => {
+            if (err) throw err
+            res.status(200).end("Deleted")
+        }
+    )
+})
+
+// Delete every item marked as completed from the database
+app.post("/todos/removeCompleted", (req, res) => {
+    let sqldelquery = `delete from items where isComplete = "1"`
+    db.query(sqldelquery, (err) => {
+        if (err) throw err
+        res.status(200).end("deleted")
+    })
+})
+
+// editSingleItemInServer: edit properties of a single item on the database
+app.post("/todos/updateTodo", (req, res) => {
+    let tempObj = req.body[0]
+    tempObj.isHidden = tempObj.isHidden == true ? 1 : 0
+    tempObj.isComplete = tempObj.isComplete == true ? 1 : 0
+    let sqlupdateQuery = `update items set todoText = "${tempObj.todoText}", isHidden = "${tempObj.isHidden}", isComplete = "${tempObj.isComplete}" where uuid = '${tempObj.uuid}'`
+    db.query(sqlupdateQuery, (err) => {
+        if (err) throw err
+        res.status(200).end("updated")
+    })
+})
+
+// Delete a single item from database
+app.post("/todos/:uuid", (req, res) => {
+    let sqldelquery = `delete from items where uuid = '${req.params.uuid}'`
+    db.query(sqldelquery, (err) => {
+        if (err) throw err
+        res.status(200).end("deleted")
+    })
+})
+
+app.listen(PORT, () => console.log("Your app is running on port ", PORT))
