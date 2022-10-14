ed5c7da2ca9a1c3105c6997e6ebf65cb0523ec9d
de-sipher <0mu5sfm4@anonaddy.me>
2022-10-14 18:35:03 +0530

    fixed issue of importing module after initilizing

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from 'cors'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-app.use(cookieParser())
 import cookie from 'cookie'
 
 dotenv.config()
